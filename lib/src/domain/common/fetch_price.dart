711034db05e3bbb2915ced9ac1c28d7f9c281b04
Saber Maram <s.maram@devcups.com>
2020-05-15 12:45:12 +0100

    api changes

--- lib/src/domain/common/fetch_price.dart
+++ lib/src/domain/common/fetch_price.dart
@@ -4,31 +4,47 @@ import 'package:cake_wallet/src/domain/common/crypto_currency.dart';
 import 'package:cake_wallet/src/domain/common/fiat_currency.dart';
 import 'package:cake_wallet/src/domain/common/currency_formatter.dart';
 
-const fiatApiAuthority = 'fiat-api.cakewallet.com';
-const fiatApiPath = '/v1/rates';
+const fiatApiAuthority = 'pro-api.coinmarketcap.com';
+const fiatApiPath = 'v1/cryptocurrency/quotes/latest';
 
 Future<double> fetchPriceFor({CryptoCurrency crypto, FiatCurrency fiat}) async {
   double price = 0.0;
 
+
   try {
+
+
+      Map<String, String> requestHeaders = {
+        'Content-Type': 'application/json',
+        'X-CMC_PRO_API_KEY': 'xxxxkey',
+      };
+      
     final fiatStringified = fiat.toString();
     final uri =
-        Uri.https(fiatApiAuthority, fiatApiPath, {'convert': fiatStringified});
-    final response = await get(uri.toString());
+        Uri.https(fiatApiAuthority, fiatApiPath, {'symbol': 'TUBE',  'convert': fiatStringified});
+    print('uri: ${uri.toString()}');
+    final response = await get(uri.toString() , headers: requestHeaders );
+
+    print('response: ${response}');
+    print('responseCode: ${response.statusCode}');
 
     if (response.statusCode != 200) {
       return 0.0;
     }
 
     final responseJSON = json.decode(response.body) as Map<String, dynamic>;
-    final data = responseJSON['data'] as List<dynamic>;
+    final data = responseJSON['data'] as Map<String, dynamic>;
 
-    for (final item in data) {
-      if (item['symbol'] == cryptoToString(crypto)) {
+    price = data['TUBE']['quote'][fiatStringified]['price'] as double;
+
+    print (data);
+
+    /*for (final item in data) {
+      if (item['symbol'] == 'TUBE') {
         price = item['quote'][fiatStringified]['price'] as double;
         break;
       }
-    }
+    }*/
 
     return price;
   } catch (e) {
